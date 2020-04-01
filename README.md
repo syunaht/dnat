d82911d1d4ab6d5f12761ad3f20ed7662b8e48f3
KagamigawaMeguri <kanomeguri@outlook.com>
2020-04-01 15:53:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+原作：https://github.com/arloor/iptablesUtils
+修改：necovm
+
+```shell
+git clone https://github.com/KagamigawaMeguri/dnat.git && cd nat && chmod +x install.sh && ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# stop service
+if [ -s /etc/systemd/system/dnat.service ] ; then
+    systemctl stop dnat
+    rm -f /etc/systemd/system/dnat.service
+fi
+
+# clean up
+rm -rf /etc/init.d/dnat
+rm -rf /usr/local/bin/dnat.sh
+rm -rf /usr/local/bin/nat
+
+echo "安装依赖...."
+apk add bash iptables &> /dev/null
+apk add iptables bind-tools &> /dev/null
+yum install -y bind-utils &> /dev/null
+apt install -y dnsutils &> /dev/null
+cp -f nat /usr/local/bin/nat && chmod +x /usr/local/bin/nat
+cp -f dnat.sh /usr/local/bin/dnat.sh && chmod +x /usr/local/bin/dnat.sh
+echo "安装依赖结束"
+
+echo "开启端口转发"
+sed -n '/^net.ipv4.ip_forward=1/'p /etc/sysctl.conf | grep -q "net.ipv4.ip_forward=1"
+if [ $? -ne 0 ]; then
+    echo -e "net.ipv4.ip_forward=1" >> /etc/sysctl.conf && sysctl -p
+fi
+
+echo "开放FORWARD链"
+arr1=(`iptables -L FORWARD -n  --line-number |grep "REJECT"|grep "0.0.0.0/0"|sort -r|awk '{print $1,$2,$5}'|tr " " ":"|tr "\n" " "`)  #16:REJECT:0.0.0.0/0 15:REJECT:0.0.0.0/0
+for cell in ${arr1[@]}
+do
+    arr2=(`echo $cell|tr ":" " "`)  #arr2=16 REJECT 0.0.0.0/0
+    index=${arr2[0]}
+    echo 删除禁止FOWARD的规则——$index
+    iptables -D FORWARD $index
+done
+iptables --policy FORWARD ACCEPT
+
+echo "注册系统服务"
+cat > /etc/systemd/system/dnat.service <<\EOF
+[Unit]
+Description=DNAT Manager
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/dnat.sh
+Restart=always
+RestartSec=30
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "启动系统服务"
+chmod 754 /etc/systemd/system/dnat.service
+systemctl daemon-reload
+systemctl enable dnat > /dev/null 2>&1
+systemctl restart dnat > /dev/null 2>&1
+echo "安装完毕，请输入指令运行 nat 运行"
